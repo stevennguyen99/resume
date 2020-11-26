2e96a7b5e31bbc121fe47210b3c42417727aad37
stevennguyen99 <40276613+stevennguyen99@users.noreply.github.com>
2020-11-26 16:51:39 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+guestname = prompt('May I have your name please?');
+random = Math.floor(Math.random() * 20);
+number = 80 + random;
+alert(guestname + ', there is ' + number + '% chance that you will like me!');
