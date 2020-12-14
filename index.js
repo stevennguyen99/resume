ad286effe6f1cfdcce516fe9f7eb726eed541901
stevennguyen99 <40276613+stevennguyen99@users.noreply.github.com>
2020-12-14 18:56:25 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 guestname = prompt('May I have your name please?');
 random = Math.floor(Math.random() * 20);
 number = 80 + random;
+
+var clap = new Audio('clap.wav');
+clap.play();
+
 alert(guestname + ', there is ' + number + '% chance that you will like me!');
